3d328d3fb6cd5898096bdb5c30e1a26b5adfc03b
JoshuaPetiunaung <162709391+JoshuaPetiunaung@users.noreply.github.com>
2025-11-09 17:31:56 +0800

    update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-red-700 text-white text-center py-8 mt-16">
-      <p className="text-sm">&copy; 2025 Desa Kiawa Satu & KKT 144 Unsrat.</p>
+      <p className="text-sm">&copy; 2025 Desa Kiawa Satu & KKT 144 Unsrat Posko Desa Kiawa Satu.</p>
     </footer>
   )
 }
